caba6418a911255a5cba7254b4cf52467e8025aa
Elvira <elvira_sosili_24rpl@student.smktelkom-mlg.sch.id>
2017-01-11 13:50:36 +0700

    start

---
+++ 09_XIRPL3_ElviraAyuSosilia_HelloWorld/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_09_XIRPL3_ElviraAyuSosilia_HelloWorld.MvcApplication" Language="C#" %>
---
+++ 09_XIRPL3_ElviraAyuSosilia_HelloWorld/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_09_XIRPL3_ElviraAyuSosilia_HelloWorld.Startup))]
+namespace _09_XIRPL3_ElviraAyuSosilia_HelloWorld
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
